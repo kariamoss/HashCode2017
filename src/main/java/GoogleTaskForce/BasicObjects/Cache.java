982612a80df33e3d6b4cde4e87483f926f977312
Kariamoss <jehanmilleret@gmail.com>
2017-02-23 19:30:47 +0100

    Add video to a cache

--- src/main/java/GoogleTaskForce/BasicObjects/Cache.java
+++ src/main/java/GoogleTaskForce/BasicObjects/Cache.java
@@ -1,12 +1,21 @@
 package GoogleTaskForce.BasicObjects;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Jehan on 23/02/2017, 19:09.
  */
 public class Cache {
     int size;
+    List<Video> videos;
 
     public Cache(int size) {
         this.size = size;
+        videos = new ArrayList<Video>();
+    }
+
+    public void addVideo(Video video){
+        videos.add(video);
     }
 }
