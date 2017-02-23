1ce12a68a00d45a8baf457749a5f06cf4bacdc49
Lucas <lucas.matteo@etu.unice.fr>
2017-02-23 20:51:00 +0100

    change maven and other changes

--- src/test/java/GoogleTaskForce/basicObjects/EndPoint.java
+++ src/test/java/GoogleTaskForce/basicObjects/EndPoint.java
@@ -1,8 +1,6 @@
 package GoogleTaskForce.basicObjects;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+
 
 public class EndPoint {
     private EndPoint point;
